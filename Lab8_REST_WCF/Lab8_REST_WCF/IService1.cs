d9db71e6f6457b7b6f9d17f7198f7b99f1d1ff25
jacKlinc <jackharding321@gmail.com>
2019-03-25 15:57:27 +0000

    Lab8 tested and working

---
+++ Lab8_REST_WCF/Lab8_REST_WCF/IService1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace Lab8_REST_WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string JSONData(string id);
+    }
+}
+
+/*
+ * 2>Unable to add 'Service1.svc' to the Web site.  Unable to add file 'Service1.svc'.  Access is denied.
+2>Unable to add 'Web.config' to the Web site.  Unable to add file 'Web.config'.  Access is denied.
+2>Unable to add folder 'bin' to the Web site.  Cannot create folder 'bin'.  Access is denied.
+2>Publish failed. Target file:///C:/inetpub/wwwroot/RESTWCFApp.
+*/
---
+++ Lab8_TestApp/Lab8_TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+namespace Lab8_TestApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            using (WebClient myClient = new WebClient()) //to talk to the REST service you need to create
+                                                         // a WebClient object and call the DownloadString
+                                                         // method of the object
+            {
+                Console.WriteLine("testing xml function\n");
+                var response =
+               myClient.DownloadString("http://localhost:51033/Service1.svc/xml/123"); // change to your port number and service file
+                Console.WriteLine(response);
+                Console.WriteLine("\n\ntesting json function\n");
+                response = myClient.DownloadString("http://localhost:51033/Service1.svc/json/456");
+                Console.WriteLine(response);
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
